edb3f0f572d5f2831f431e3f910a94266d1605ab
Dhani <Dhani@Dhani-2.local>
2019-03-10 22:34:04 +0100

    change document icon

--- frontend/src/pages/TenantDocument.js
+++ frontend/src/pages/TenantDocument.js
@@ -3,8 +3,7 @@ import {connect} from "react-redux";
 import Aux from '../hoc/Aux_';
 import Navbar from '../components/Navbar';
 import {getTenantDocuments} from "../store/actions/tenant";
-import {Image, Video, Transformation, CloudinaryContext} from 'cloudinary-react';
-import {Redirect} from "react-router-dom";
+import {Link, Redirect} from "react-router-dom";
 
 
 class TenantDocument extends React.Component {
@@ -28,23 +27,20 @@ class TenantDocument extends React.Component {
   }
 
   render() {
-    const { isAuthenticated } = this.props;
-     if (!isAuthenticated) {
-      return <Redirect to="/login" />
+    const {isAuthenticated} = this.props;
+    if (!isAuthenticated) {
+      return <Redirect to="/login"/>
     }
     return (
       <Aux>
         <Navbar/>
         <section className="section section-lg bg-web-desc">
           <div className="bg-overlay"></div>
-          <div className="bg-pattern-effect">
-            <img src="images/bg-pattern.png" alt=""/>
-          </div>
         </section>
         <section className="section pt-4" id="team">
           <div className="container">
             {/*<div className="loading text-center">*/}
-              {/*{this.state.isLoading && "Loading Documents..."}*/}
+            {/*{this.state.isLoading && "Loading Documents..."}*/}
             {/*</div>*/}
             <div className="row">
               <div className="col-lg-8 offset-lg-2">
@@ -52,30 +48,26 @@ class TenantDocument extends React.Component {
                 <div className="section-title-border margin-t-20"></div>
                 <p className="section-subtitle text-muted text-center font-secondary padding-t-30">It is a long
                   established fact that a reader will be distracted by the readable content of a page when looking at
-                  its layout.</p>
+                  its layout.
+                </p>
               </div>
-              <div className="row margin-t-50">
+            </div>
+            <div className="row margin-t-50">
                 {
-                  this.state.documents && this.state.documents.map((item) =>  {
+                  this.state.documents && this.state.documents.map((item) => {
                     return (
-                       <div className="col-lg-3 col-sm-6">
-                        <div className="team-box text-center hover-effect">
-                            <div className="team-wrapper">
-                                <div className="team-member">
-                                    <img alt="" src="images/team/img-1.png" className="img-fluid rounded" />
-                                </div>
-                            </div>
-                            <h4 className="team-name"></h4>
-                            <p className="text-uppercase team-designation">
-                              <a href={item.document}>{item.name}</a>
-                            </p>
+                      <div className="col-lg-3 margin-t-20">
+                        <div className="documents-box text-center hover-effect">
+                          <i className="pe-7s-folder text-custom"></i>
+                          <h4 className="padding-t-15 portal-heading">
+                            <a href={item.document}>{item.name}</a>
+                          </h4>
                         </div>
-                    </div>
+                      </div>
                     )
                   })
                 }
-                </div>
-            </div>
+              </div>
           </div>
         </section>
       </Aux>
