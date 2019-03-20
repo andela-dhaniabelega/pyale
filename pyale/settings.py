5133116b691b532bf9a45ff9444621336db27ed5
Dhani <Dhani@Dhani-2.local>
2019-03-20 18:56:20 +0100

    Run python manage.py collectstatic --noinput locally

--- pyale/settings.py
+++ pyale/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'xi0^o_&%3-^w$k=pdv4g6j3p^81ehurhvhsdt)2f4mhax676oi'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
