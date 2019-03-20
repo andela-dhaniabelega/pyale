af5c0eac397c55b5e916218ad9e349e64522c199
Dhani <Dhani@Dhani-2.local>
2019-03-20 21:14:27 +0100

    Remove redux code

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -14,5 +14,5 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-export const store = createStore(persistedReducer, compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
+export const store = createStore(persistedReducer, compose(applyMiddleware(thunk)));
 export const persistor = persistStore(store);
